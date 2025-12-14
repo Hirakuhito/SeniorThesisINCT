f8313c205f7a569562493da15dee8910c821a5d6
Hirakuhito <takuto.yoshino1210@icloud.com>
2025-12-14 17:04:52 +0900

    -

---
+++ main/trackMaker/point_info_generator.py
@@ -0,0 +1,13 @@
+"""
+This program is designed exclusively for Oval circuit.
+"""
+
+def gen_center_point(length, radius):
+    """
+    params:
+        length (float) : straight length of circuit.
+        radius (float) : corner radius of circuit.
+
+    return:
+        points (list) : center line points.
+    """
