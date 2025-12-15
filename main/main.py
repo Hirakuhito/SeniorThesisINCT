6b2fa22dc471a1fb90decad0558becad9b3ccc16
Hirakuhito <takuto.yoshino1210@icloud.com>
2025-12-15 14:08:41 +0900

    -

--- main/main.py
+++ main/main.py
@@ -3,11 +3,14 @@ import time
 import numpy as np
 import pybullet as p
 import pybullet_data as pd
+import trackMaker.point_info_generator as pg
 
 
 def main():
     print("# Welcome")
 
+    a = pg.gen_center_point(100, 50)
+    print(f"生成されたポイントの総数: {len(a)}")
     
     
 
--- main/trackMaker/point_info_generator.py
+++ main/trackMaker/point_info_generator.py
@@ -19,7 +19,7 @@ def gen_center_point(length, radius, segments=100, pos=np.array([0, 0])):
     if length < 0 or radius < 0 or segments < 0:
         raise ValueError("'length', 'radius' and 'segments' must be positive number.")
     
-    if isinstance(segments, int):
+    if not isinstance(segments, int):
         raise TypeError("Segments must be integer.")
 
     #*=============== vailables ====================
@@ -30,9 +30,41 @@ def gen_center_point(length, radius, segments=100, pos=np.array([0, 0])):
     corner_offset = length / 2 #* center of cerner section [0, coner_offset]
 
     #*============== sections ======================
-    #* Upper straight
-    y_straight = np.linspace(corner_offset, -corner_offset, segments, endpoint=True)
+    #* left straight
+    y_straight = np.linspace(-corner_offset, corner_offset, segments, endpoint=True)
     x_straight = np.full_like(y_straight, radius)
-    upper_straight = np.stack((x_straight, y_straight), axis=1)
+    left_straight = np.stack((x_straight, y_straight), axis=1)
 
-    all_points.append(upper_straight)
+    all_points.append(left_straight)
+
+    #* upper corner
+    angle = np.linspace(np.pi, 0, segments, endpoint=True)
+
+    x_arc_U = radius * np.cos(angle)
+    y_arc_U = radius * np.sin(angle)
+
+    arc_U = np.stack((x_arc_U, y_arc_U + corner_offset), axis=1)
+
+    all_points.append(arc_U)
+
+    #* right straight
+    y_straight = np.linspace(corner_offset, -corner_offset, segments, endpoint=True)
+    x_straight = np.full_like(y_straight, -radius)
+    right_straight = np.stack((x_straight, y_straight), axis=1)
+
+    all_points.append(right_straight)
+
+    #* lower corner
+    angle = np.linspace(0, np.pi, segments, endpoint=False)
+
+    x_arc_L = radius * np.cos(angle)
+    y_arc_L = -radius * np.sin(angle)
+
+    arc_L = np.stack((x_arc_U, y_arc_U + corner_offset), axis=1)
+
+    all_points.append(arc_L)
+
+    points = np.concatenate(all_points, axis=0)
+    points += pos
+
+    return points
